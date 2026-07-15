05ace5466630a1f19d9fb9ceb7204888494b10cc
Akankhya Sahoo <akankhya.sahoo182002@gmail.com>
2026-07-15 17:13:06 +0530

    Initial commit

---
+++ src/config/db.js
@@ -0,0 +1,18 @@
+const dns = require("node:dns");
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
+
+const mongoose = require("mongoose");
+
+function connectToDB() {
+  mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => {
+      console.log("Server is connected");
+    })
+    .catch((err) => {
+      console.log("Error connecting to DB");
+      process.exit(1);
+    });
+}
+
+module.exports = connectToDB;
